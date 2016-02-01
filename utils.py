8fbb696c9ee663a00df5e5e341371dad01ae3511
drumber-1 <drumber01@gmail.com>
2016-02-01 23:25:20 +0000

    case checking

--- utils.py
+++ utils.py
@@ -11,3 +11,18 @@ def read_responses(fname):
     for line in f:
         responses.append(line)
     return responses
+    
+def correct_case(input_string):
+	input_string = input_string.lower()
+	output_string = ""
+	capitalise = True
+	for c in input_string:
+		if capitalise and c.isalpha():
+			output_string += c.upper()
+			capitalise = False
+		else:
+			output_string += c
+			if c is "." or c is "?" or c is "!":
+				capitalise = True
+	return output_string
+		
