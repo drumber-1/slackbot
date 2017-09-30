309501758862d1718fab322cdab60cc0a0051b26
drumber-1 <drumber01@gmail.com>
2017-09-30 20:12:14 +0100

    Add coin bot first pass

--- coinbot/coinbot.py
+++ coinbot/coinbot.py
@@ -1,6 +1,7 @@
 import commandbot
 import stocks
 
+
 class Account(object):
     def __init__(self, user_id, starting_cash):
         self.user_id = user_id
@@ -10,43 +11,41 @@ class Account(object):
 
 class CoinBot(commandbot.CommandBot):
     def __init__(self, api_key, channel):
-        super(CoinBot, self).__init__(api_key, channel, "cb", description="A bot for playing hangmanbot!")
+        super(CoinBot, self).__init__(api_key, channel, "cb", description="Bitcoin buying game!")
+
+        self.leader_emoji = ":crown:"
+        self.trailer_emoji = ":poop:"
 
         # Game data
-        self.stocks = stocks.Stocks()
+        self.stocks = stocks.Stocks("coinbot/accounts.json")
+        self.stocks.load_from_file()
 
         # Commands
-        self.command_system.add_command("join", self.add_user, "Create an account", requires_user=True)
+        self.command_system.add_command("register", self.add_user, "Create an account", requires_user=True)
         self.command_system.add_command("buy", self.buy, "Buy some coin <$ amount>", requires_user=True, has_args=True)
         self.command_system.add_command("sell", self.sell, "Sell some coin <btc amount>", requires_user=True, has_args=True)
         self.command_system.add_command("price", self.check_price, "Check price")
+        self.command_system.add_command("account", self.check_account, "Check your account", requires_user=True)
+        self.command_system.add_command("leaderboard", self.leaderboard, "See who is best", requires_user=False)
 
     def add_user(self, slack_user):
-        if slack_user.id in self.user_accounts:
+        if self.stocks.add_account(slack_user.id, slack_user.name):
+            self.saypush("Welcome {user}, you have been given ${cash}, don't spend it all at once!\n".format(
+                user=slack_user.name, cash=self.stocks.initial_cash))
+        else:
             self.saypush("You are already playing stupid\n")
-            return
-        self.user_accounts[slack_user.id] = Account(slack_user.id, self.initial_cash)
-        self.saypush("Welcome {user}, you have been given ${cash}, don't spend it all at once!\n".format(user=slack_user.name, cash=self.initial_cash))
-
-    def check_user(self, slack_user):
-        if slack_user.id not in self.user_accounts:
-            self.add_user(slack_user)
 
     def buy(self, slack_user, args):
-        self.check_user(slack_user)
-
         if len(args) == 0:
             self.saypush("You have to give an amount in $")
             return
-
-        account = self.user_accounts[slack_user.id]
 
         buy_dollars = 0
         try:
             buy_dollars = float(args[0])
         except ValueError:
             if args[0].lower() == "all":
-                buy_dollars = account.cash
+                buy_dollars = self.stocks.accounts[slack_user.id].cash
             else:
                 self.saypush("{} is not a valid $ value".format(args[0]))
                 return
@@ -55,37 +54,29 @@ class CoinBot(commandbot.CommandBot):
             self.saypush("{} is not a valid $ value".format(args[0]))
             return
 
-            buy_dollars = round(buy_dollars, 2)
+        result = self.stocks.buy(slack_user.id, buy_dollars)
+        self.stocks.save_to_file()
 
-        if buy_dollars > account.cash:
-            self.saypush("You don't have {}".format(buy_dollars))
-            return
-
-        btc_bought = buy_dollars / self.stocks.get_price_bitcoin()
-        btc_bought /= 1 + self.transaction_fee
-
-        self.saypush("Buying {} btc for ${}, including a {}% transaction fee".format(btc_bought, buy_dollars, 100 * self.transaction_fee))
-        account.cash -= buy_dollars
-        account.bitcoin += btc_bought
-
+        if result.error == stocks.StockReturnCodes.Good:
+            self.saypush("-${} | +{} btc ({} btc transaction fee)".format(abs(result.cash), result.coin, result.transaction_fee))
+        elif result.error == stocks.StockReturnCodes.NoFundsError:
+            self.saypush("You don't have ${}".format(buy_dollars))
+        elif result.error == stocks.StockReturnCodes.NoAccountError:
+            self.saypush("Please create an account using \"cb: register\"")
+        else:
+            self.saypush("UNKNOWN ERROR")
 
     def sell(self, slack_user, args):
-        self.check_user(slack_user)
-
-        self.check_user(slack_user)
-
         if len(args) == 0:
-            self.saypush("You have to give an amount in $")
+            self.saypush("You have to give an amount in btc")
             return
-
-        account = self.user_accounts[slack_user.id]
 
         sell_btc = 0
         try:
             sell_btc = float(args[0])
         except ValueError:
             if args[0].lower() == "all":
-                sell_btc = account.bitcoin
+                sell_btc = self.stocks.accounts[slack_user.id].bitcoin
             else:
                 self.saypush("{} is not a valid btc value".format(args[0]))
                 return
@@ -94,18 +85,38 @@ class CoinBot(commandbot.CommandBot):
             self.saypush("{} is not a valid btc value".format(args[0]))
             return
 
-        if sell_btc > account.bitcoin:
-            self.saypush("You don't have {}".format(sell_btc))
-            return
+        result = self.stocks.sell(slack_user.id, sell_btc)
+        self.stocks.save_to_file()
 
-        dollars_received = sell_btc / self.stocks.get_price_bitcoin()
-        dollars_received /= 1 + self.transaction_fee
-
-        self.saypush("Selling {} btc for ${}, including a {}% transaction fee".format(sell_btc, dollars_received, 100 * self.transaction_fee))
-        account.cash += dollars_received
-        account.bitcoin -= sell_btc
+        if result.error == stocks.StockReturnCodes.Good:
+            self.saypush("+${} | {} btc ({} btc transaction fee)".format(result.cash, result.coin, result.transaction_fee))
+        elif result.error == stocks.StockReturnCodes.NoFundsError:
+            self.saypush("You don't have {} btc".format(sell_btc))
+        elif result.error == stocks.StockReturnCodes.NoAccountError:
+            self.saypush("Please create an account using \"cb: register\"")
+        else:
+            self.saypush("UNKNOWN ERROR")
 
     def check_price(self):
         price = self.stocks.get_price_bitcoin()
         self.saypush("1 btc = ${price}".format(price=price))
 
+    def check_account(self, slack_user):
+        if slack_user.id not in self.stocks.accounts:
+            self.saypush("Please create an account using \"cb: register\"")
+        else:
+            self.saypush(self.stocks.account_summary_string(slack_user.id))
+
+    def leaderboard(self):
+        message = "Current scores:\n"
+
+        sorted_ids = self.stocks.sorted_user_ids()
+
+        for k in sorted_ids:
+            emoji = ""
+            if k is sorted_ids[0]:
+                emoji = self.leader_emoji
+            elif k is sorted_ids[-1]:
+                emoji = self.trailer_emoji
+            message += "\t" + emoji + " " + self.stocks.account_summary_string(k) + "\n"
+        self.saypush(message)
--- coinbot/stocks.py
+++ coinbot/stocks.py
@@ -1,49 +1,76 @@
 import utils
+import json
+import os
 
 
 class StockReturnCodes:
-    Good, NoFundsError, MiscError = range(3)
+    Good, NoFundsError, NoAccountError, MiscError = range(4)
 
 
 class Account(object):
-    def __init__(self, user_id, starting_cash):
-        self.user_id = user_id
+    def __init__(self, name, starting_cash, data=None):
+        self.name = name
         self.cash = starting_cash
         self.bitcoin = 0
+        self.load(data)
+
+    def load(self, data):
+        self.name = data["name"]
+        self.cash = data["cash"]
+        self.bitcoin = data["bitcoin"]
 
 
 class Transaction(object):
-    def __init__(self, cash, coin, code):
+    def __init__(self, cash, coin, fee, code):
         self.cash = cash
         self.coin = coin
         self.error = code
+        self.transaction_fee = fee
 
 
 class Stocks(object):
 
-    def __init__(self):
+    def __init__(self, save_file):
         self.accounts = {}
         self.transaction_fee_fraction = 0.03
         self.initial_cash = 100
 
-    def add_account(self, id):
+        self.save_file = save_file
+
+    def add_account(self, id, name):
         if id in self.accounts:
             return False
         else:
-            self.accounts[id] = Account(id, self.initial_cash)
+            self.accounts[id] = Account(name, self.initial_cash)
             return True
 
     def buy(self, id, cash_value):
+        if id not in self.accounts:
+            return Transaction(0, 0, 0, StockReturnCodes.NoAccountError)
         account = self.accounts[id]
         if cash_value > account.cash:
-            return Transaction(0, 0, StockReturnCodes.NoFundsError)
+            return Transaction(0, 0, 0, StockReturnCodes.NoFundsError)
+        cash_value = round(cash_value, 2)
         btc_value = cash_value / self.get_price_bitcoin()
         transaction_fee_btc = btc_value * self.transaction_fee_fraction
 
         account.cash -= cash_value
         account.bitcoin += btc_value - transaction_fee_btc
-        return StockReturnCodes.Good
-        #return "You bought {} btc ( {} btc - {} btc transaction fee ) for $ {}".format(btc_value - transaction_fee_btc, btc_value, transaction_fee_btc, cash_value)
+        return Transaction(-cash_value, btc_value - transaction_fee_btc, transaction_fee_btc, StockReturnCodes.Good)
+
+    def sell(self, id, btc_value):
+        if id not in self.accounts:
+            return Transaction(0, 0, StockReturnCodes.NoAccountError)
+        account = self.accounts[id]
+        transaction_fee_btc = btc_value * self.transaction_fee_fraction
+        if btc_value > account.bitcoin:
+            return Transaction(0, 0, 0, StockReturnCodes.NoFundsError)
+        cash_value = (btc_value - transaction_fee_btc) * self.get_price_bitcoin()
+        cash_value = round(cash_value, 2)
+
+        account.cash += cash_value
+        account.bitcoin -= btc_value
+        return Transaction(cash_value, -btc_value, transaction_fee_btc, StockReturnCodes.Good)
 
     def get_price_bitcoin(self):
         url = 'https://api.coindesk.com/v1/bpi/currentprice/{}.json'.format("USD")
@@ -51,3 +78,30 @@ class Stocks(object):
         price = data['bpi']["USD"]['rate']
 
         return float(price.replace(",", ""))
+
+    def save_to_file(self):
+        fout = open(self.save_file, 'w')
+        data = {}
+        for key, value in self.accounts.items():
+            data[key] = value.__dict__
+        json.dump(data, fout, indent=4)
+
+    def load_from_file(self):
+        if not os.path.isfile(self.save_file):
+            return
+
+        fin = open(self.save_file, 'r')
+        data = json.load(fin)
+        for key, value in data.items():
+            self.accounts[key] = Account("blank", 0, data=value)
+
+    def sorted_user_ids(self):
+        sorted_ids = sorted(self.accounts.keys(), key=self.account_worth, reverse=True)
+        return sorted_ids
+
+    def account_worth(self, id):
+        return self.accounts[id].cash + self.accounts[id].bitcoin * self.get_price_bitcoin()
+
+    def account_summary_string(self, id):
+        return "{} : ${} (${} + {} btc)".format(self.accounts[id].name, self.account_worth(id), self.accounts[id].cash, self.accounts[id].bitcoin)
+
